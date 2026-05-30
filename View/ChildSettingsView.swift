eb8486520c60b2ad48dca3918935b4032fb21b7a
Rq <iwliluqp@gmail.com>
2026-05-31 02:23:49 +0300

    Edits

---
+++ View/ChildSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  ChildSettingsView.swift
+//  nafaqati
+//
+//  Created by Rama AlQahtani on 13/12/1447 AH.
+//
+
+import SwiftUI
+
+struct ChildSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChildSettingsView()
+}
