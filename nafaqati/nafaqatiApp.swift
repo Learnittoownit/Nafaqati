3165ea44273b3dc1086c4bd81e80ceff0e942f55
YaraAlsabti8 <Yara.alotaibyy@gmail.com>
2026-06-07 15:28:20 +0300

    First

--- nafaqati/nafaqatiApp.swift
+++ nafaqati/nafaqatiApp.swift
@@ -10,88 +10,91 @@ struct nafaqatiApp: App {
 
     var body: some Scene {
         WindowGroup {
-            if showSplash {
-                SplashView(showSplash: $showSplash)
+            Group {
+                if showSplash {
+                    SplashView(showSplash: $showSplash)
 
-            } else if isChildLoggedIn {
-                ChildTabView()
+                } else if isChildLoggedIn {
+                    ChildTabView()
 
-            } else if authVM.isLoggedIn {
-                ParentTabView()
-                    .environmentObject(authVM)
-                    .environmentObject(parentVM)
+                } else if authVM.isLoggedIn {
+                    ParentTabView()
+                        .environmentObject(authVM)
+                        .environmentObject(parentVM)
 
-            } else {
-                NavigationStack(path: $path) {
-                    WelcomeView(path: $path)
-                        .navigationDestination(
-                            for: OnboardingStep.self
-                        ) { step in
-                            switch step {
+                } else {
+                    NavigationStack(path: $path) {
+                        WelcomeView(path: $path)
+                            .navigationDestination(
+                                for: OnboardingStep.self
+                            ) { step in
+                                switch step {
 
-                            case .roleSelection:
-                                RoleSelectionView(path: $path)
+                                case .roleSelection:
+                                    RoleSelectionView(path: $path)
 
-                            case .parentInfo:
-                                ParentInfoView(path: $path)
+                                case .parentInfo:
+                                    ParentInfoView(path: $path)
+                                        .environmentObject(authVM)
+
+                                case .createPassword(
+                                    let name,
+                                    let email,
+                                    let numberOfChildren):
+                                    CreatePasswordView(
+                                        path: $path,
+                                        name: name,
+                                        email: email,
+                                        numberOfChildren:
+                                            numberOfChildren)
                                     .environmentObject(authVM)
 
-                            case .createPassword(
-                                let name,
-                                let email,
-                                let numberOfChildren):
-                                CreatePasswordView(
-                                    path: $path,
-                                    name: name,
-                                    email: email,
-                                    numberOfChildren:
-                                        numberOfChildren)
-                                .environmentObject(authVM)
-
-                            case .addChild(
-                                let childIndex,
-                                let totalChildren):
-                                AddChildView(
-                                    path: $path,
-                                    childIndex: childIndex,
-                                    totalChildren: totalChildren)
-                                .environmentObject(authVM)
-
-                            case .myChildren:
-                                MyChildrenView(path: $path)
+                                case .addChild(
+                                    let childIndex,
+                                    let totalChildren):
+                                    AddChildView(
+                                        path: $path,
+                                        childIndex: childIndex,
+                                        totalChildren: totalChildren)
                                     .environmentObject(authVM)
 
-                            case .childPIN:
-                                ChildPINView(
-                                    path: $path,
-                                    isLoginMode: true)
-                                    .environmentObject(authVM)
+                                case .myChildren:
+                                    MyChildrenView(path: $path)
+                                        .environmentObject(authVM)
 
-                            case .parentSetPIN:
-                                ChildPINView(
-                                    path: $path,
-                                    isLoginMode: false)
-                                    .environmentObject(authVM)
+                                case .childPIN:
+                                    ChildPINView(
+                                        path: $path,
+                                        isLoginMode: true)
+                                        .environmentObject(authVM)
 
-                            case .allSet:
-                                AllSetView(path: $path)
-                                    .environmentObject(authVM)
-                                    .environmentObject(parentVM)
+                                case .parentSetPIN:
+                                    ChildPINView(
+                                        path: $path,
+                                        isLoginMode: false)
+                                        .environmentObject(authVM)
 
-                            case .login:
-                                LoginView(path: $path)
-                                    .environmentObject(authVM)
+                                case .allSet:
+                                    AllSetView(path: $path)
+                                        .environmentObject(authVM)
+                                        .environmentObject(parentVM)
 
-                            case .forgotPassword:
-                                ForgotPasswordView(path: $path)
-                                    .environmentObject(authVM)
+                                case .login:
+                                    LoginView(path: $path)
+                                        .environmentObject(authVM)
+
+                                case .forgotPassword:
+                                    ForgotPasswordView(path: $path)
+                                        .environmentObject(authVM)
+                                }
                             }
-                        }
+                    }
+                    .environmentObject(authVM)
+                    .onAppear { path = NavigationPath() }
+                    .task { await authVM.checkSession() }
                 }
-                .environmentObject(authVM)
-                .onAppear { path = NavigationPath() }
-                .task { await authVM.checkSession() }
             }
+            .preferredColorScheme(.light)
         }
     }
 }
